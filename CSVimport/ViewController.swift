16f43e7601e1a735adfe8f77271ff8e0c1a87156
Bagas Ilham <rabbanilham@gmail.com>
2023-02-05 20:29:29 +0700

    Initial Commit

---
+++ CSVimport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSVimport
+//
+//  Created by Bagas Ilham on 05/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
